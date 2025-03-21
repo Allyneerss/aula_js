7fceac828bb954fcaf52c489ba6fafeb4cfcaf3d
Allyneerss <draalineramos@hotmail.com>
2025-03-20 21:03:41 -0300

    commit3

---
+++ main.js
@@ -0,0 +1,9 @@
+const array = [2,3,4,5,6];
+array.forEach(item =>{
+    if(item % 2 === 0){
+        console.log(`o numero ${item} é par` );
+    }
+    else{
+        console.log(`o numero ${item} é impar` );
+    }
+    });
